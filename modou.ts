c9e44537a75e21ac9fc83030ea3ae4cd915ecb70
jiarui.sun <jiarui.sun@hmpt-ms.com>
2020-06-22 00:01:48 +0800

    remove ir files from extension

--- modou.ts
+++ modou.ts
@@ -410,24 +410,3 @@ namespace Modou_Motion {
         }
     }
 }
-
-/*
-//% color=#009ede icon="\uf110"
-namespace Modou_IR_T {
-    
-    //% advanced=true shim=Modou_IR::onPressEvent
-    function onPressEvent(btn: RemoteButton, body: Action): void {
-        return;
-    }
-
-    /**
-     * set button pressed event handler.
-     */
-    //% blockId=modou_ir_button_pressed_event_user
-    //% block="on button |%btn| pressed"
-/*
-export function onPressEventUser(btn: RemoteButton, body: Action): void { 
-        onPressEvent(btn, body);       
-    }
-    
-}*/
